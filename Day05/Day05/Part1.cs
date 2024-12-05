00ea6984b906e9e690d7bacfeac51716a843a7b2
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-12-05 18:03:20 +0200

    Add Day05, part 1

---
+++ Day05/Day05/Part1.cs
@@ -0,0 +1,51 @@
+namespace Day05;
+
+public class Part1
+{
+    private static (List<(int, int)> Rules, List<List<int>> Publications) ParseInput(string input)
+    {
+        var parts = input.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+        var rules = parts[0]
+            .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(rule =>
+            {
+                var numbers = rule.Split('|').Select(int.Parse).ToArray();
+                return (numbers[0], numbers[1]);
+            })
+            .ToList();
+
+
+        var publications = parts[1]
+            .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(publication =>
+                publication.Split(',')
+                    .Select(int.Parse)
+                    .ToList()
+            )
+            .ToList();
+
+        return (rules, publications);
+    }
+
+    private static bool IsValidPublication(List<int> publication, List<(int, int)> rules)
+    {
+        foreach (var rule in rules)
+            if (publication.Contains(rule.Item1) && publication.Contains(rule.Item2))
+                if (publication.IndexOf(rule.Item1) > publication.IndexOf(rule.Item2))
+                    return false;
+
+        return true;
+    }
+
+    public static int SolvePart1(string input)
+    {
+        var (rules, publications) = ParseInput(input);
+        
+        return publications
+            .Where(publication => IsValidPublication(publication, rules))
+            .Select(list => list[list.Count / 2])
+            .Sum();
+    }
+}
---
+++ Day05/Day05/Program.cs
@@ -0,0 +1,8 @@
+﻿using Day05;
+
+var directory = AppDomain.CurrentDomain.BaseDirectory;
+var filePath = Path.Combine(directory, "input.txt");
+
+var input = File.ReadAllText(filePath);
+
+Console.WriteLine("Part 1: " + Part1.SolvePart1(input));
